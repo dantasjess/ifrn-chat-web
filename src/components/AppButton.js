94c89c253650375eca13b34692eeec8bbba36b4b
Joanderson Freire <joanderson.santos7@gmail.com>
2022-05-25 17:46:29 -0300

    index.js modificado para landing page e adicionado botão de voltar em login.js

---
+++ src/components/AppButton.js
@@ -0,0 +1,5 @@
+import React from 'react';
+export const AppButton = props => (
+<div>
+    <input type="submit" title={props.title} />
+</div>);
---
+++ src/components/AppInput.js
@@ -0,0 +1,6 @@
+import React from 'react';
+export const AppInput = props => (
+<div>
+    <label>{props.label}</label>
+    <input type={props.type} title={props.title} />
+</div>);
