b66dd433210eb0fc8cc286acd96b4769c680d615
Joanderson Freire <joanderson.santos7@gmail.com>
2022-05-25 17:22:07 -0300

    adicionado botão de voltar em login.js

--- pages/login.js
+++ pages/login.js
@@ -1,40 +1,31 @@
 import React from 'react';
-
-const AppContainer=(props) => <div>{props.children}</div>;
-const AppInput = props => (
-	<div>
-		<label>{props.label}</label>
-		<input type={props.type} title={props.title} />
-	</div>
-);
-const AppButton = props => (
-	<div>
-		<input type="submit" title={props.title} />
-	</div>
-);
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { AppContainer } from '../src/components/AppContainer';
+import { AppInput } from '../src/components/AppInput';
+import { AppButton } from '../src/components/AppButton';
 
 const Login = props => {
+  const router = useRouter();
+
   return (
+    <div>
+      <Head>
+        <title>página de login</title>
+      </Head>
+      <main>
+        <nav>
+          <p>Chat de Infoweb</p>
+          <button onClick={() => router.back()}>voltar</button>
+        </nav>
         <AppContainer>
-          <AppInput
-            title="apelido ou email do usuário"
-            label="Usuário"
-            type="text"
-          />
-          <AppInput 
-          label="Senha:" 
-          type="password" 
-          title="senha do usuário" 
-        />
-          <AppButton title = "Entrar" />
+          <AppInput label="Usuário" title="apelido ou email do usuário"/>
+          <AppInput label="Senha" type="password" />
+          <AppButton title="Clique aqui para entrar no chat" label="Entrar no chat" />
         </AppContainer>
+      </main>
+      </div>
   );
 };
 
 export default Login;
-/*
-html 
-  head
-  body
-
-*/
